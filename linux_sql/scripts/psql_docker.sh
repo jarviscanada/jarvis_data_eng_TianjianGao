c6981fe585f9d8a2e5b13e6594da4c7ae97e68a7
Tianjian Gao <tianjian.lucas.gao@gmail.com>
2024-08-11 01:59:40 +0000

    Completed host_info.sh and host_usage.sh, updated psql_docker.sh

--- linux_sql/scripts/psql_docker.sh
+++ linux_sql/scripts/psql_docker.sh
@@ -1,53 +1,50 @@
-#! /bin/sh
+#!/bin/sh
 
-<<<<<<< HEAD
-echo "hello world"
-name=$1
-
-exit 0
-=======
+# variables
 cmd=$1
 db_username=$2
 db_password=$3
 
-sudo systemctl status docker || systemctl start docker
+# start docker
+sudo systemctl status docker > /dev/null || systemctl start docker
 
-docker container inspect jrvs-psql
+docker container inspect jrvs-psql > /dev/null
 container_status=$?
 
 case $cmd in
-	create)
+  create)
 
-	if [$container_status -eq 0]; then
-		echo 'Container already exists'
-		exit 1
-	fi
+    if [ $container_status -eq 0 ]; then
+      echo 'Container already exists'
+      exit 1
+    fi
 
-	if [$# -ne 3]; then
-		echo 'Create requires username and password'
-		exit 1
-	fi
+    if [ $# -ne 3 ]; then
+      echo 'Create requires username and password'
+      exit 1
+    fi
+    
+    # create volume pgdata
+    docker volume create pgdata
+    
+    docker run --name jrvs_psql -e POSTGRES_PASSWORD=$db_password -e POSTGRES_USER=$db_username -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres:9.6-alpine
+    exit $?
+    ;;
 
-	docker volume create pgdata
-	docker run --name jrvs_psql -e POSTGRES_PASSWORD=$db_password -e POSTGRES_USER=$db_username -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres:9.6-alpine
-	exit $?
-	;;
+  start|stop)
 
-	start|stop)
+    if [ $container_status -eq 1 ]; then
+      echo 'Container has not been created'
+      exit 1
+    fi
 
-	if [$container_status -eq 1]; then
-		echo 'Container has not been created'
-		exit 1
-	fi
+    docker container $cmd jrvs-psql
+    exit $?
+    ;;
 
-	docker container $cmd jrvs-psql
-	exit $?
-	;;
-
-	*)
-	echo 'Illegal command'
-	echo 'Commands: start|stop|create'
-	exit 1
-	;;
+  *)
+    echo 'Illegal command'
+    echo 'Commands: start|stop|create'
+    exit 1
+    ;;
 esac
->>>>>>> feature/psql_docker
